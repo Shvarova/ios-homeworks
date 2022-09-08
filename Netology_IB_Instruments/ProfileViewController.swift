c6427cbe8d34b3b0c420be80ba0c012e7a7d7654
shvarova <di.shvarova@gmail.com>
2022-09-08 21:18:19 +0300

    HW-1 версия 1

---
+++ Netology_IB_Instruments/ProfileViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Дина Шварова on 08.09.2022.
+//
+
+import Foundation
+import UIKit
+
+class ProfileViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
